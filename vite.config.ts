de7cd31702e53d2e7b5d96e5b9a01340c8d1e20e
israel zusha rotenberg <100128949+zushar@users.noreply.github.com>
2024-07-20 19:41:12 +0300

    refactor: Update fetchLeaderboard URL to use relative path

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-      proxy: {
-        '/leaderboard': {
-          target: 'https://baas-data-provider.onrender.com',
-          changeOrigin: true,
-          secure: false,
-      },
-    },
-  },
+  // server: {
+  //     proxy: {
+  //       '/leaderboard': {
+  //         target: 'https://baas-data-provider.onrender.com',
+  //         changeOrigin: true,
+  //         secure: false,
+  //     },
+  //   },
+  // },
 })
  // const LEADERBOARD_ROUTE =
   //   'https://baas-data-provider.onrender.com/leaderboard';

2070ecece8007ca344df1c7367ede6682a8116f2
israel zusha rotenberg <100128949+zushar@users.noreply.github.com>
2024-07-20 15:35:21 +0300

    refactor: Replace App component with LeaderboardPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-
+import LeaderboardPage from "./pages/Leaderboard"
 function App() {
   
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <LeaderboardPage />
   )
 }
 

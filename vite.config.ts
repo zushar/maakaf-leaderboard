81f2da55b3cd6532b4eea4628d441bc57a9b4701
israel zusha rotenberg <100128949+zushar@users.noreply.github.com>
2024-07-20 19:04:13 +0300

    chore: Update fetchLeaderboard URL to use HTTPS

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-      proxy: {
-        '/leaderboard': {
-          target: 'http://localhost:8080',
-          changeOrigin: true,
-          secure: false,
-      },
-    },
-  },
+  // server: {
+  //     proxy: {
+  //       '/leaderboard': {
+  //         target: 'http://localhost:8080',
+  //         changeOrigin: true,
+  //         secure: false,
+  //     },
+  //   },
+  // },
 })
  // const LEADERBOARD_ROUTE =
   //   'https://baas-data-provider.onrender.com/leaderboard';

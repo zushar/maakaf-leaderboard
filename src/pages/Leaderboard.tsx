ca0ec3f1107c2b560234ff4759c2bab7e5386978
israel zusha rotenberg <100128949+zushar@users.noreply.github.com>
2024-07-20 16:34:27 +0300

    chore: Update dependencies and remove unused CSS code

--- src/pages/Leaderboard.tsx
+++ src/pages/Leaderboard.tsx
@@ -15,9 +15,9 @@ const LeaderboardPage: React.FC = () => {
 
   return (
     <>
-      <h3 className="text-2xl font-bold mb-2 text-center">Our Contributions In Maakaf</h3>
-      <div className="sticky top-20 dark:bg-gray-600 z-10 shadow-md">
-        <div className="font-bold flex flex-row justify-center p-4">
+      <div className="sticky top-0 bg-gray-600 z-10 shadow-md text-white">
+      <h3 className="text-2xl font-bold text-center">Our Contributions In Maakaf</h3>
+        <div className="font-bold flex flex-row justify-center p-2">
           <a href={"#allTimes"} className="transition duration-300 group px-4">
             <span>{"All Times Contribution"}</span>
             <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600" />
@@ -32,8 +32,8 @@ const LeaderboardPage: React.FC = () => {
           </a>
         </div>
       </div>
-      <ContributionsList data={contributors3} key={`contributors3`} />
       <ContributionsList data={contributors2} key={`contributors2`} />
+      <ContributionsList data={contributors3} key={`contributors3`} />
       <ContributionsList data={contributors} key={`contributors`} />
     </>
   )
@@ -51,10 +51,10 @@ const ContributionsList: React.FC<{ data: ContributorsInsights }> = ({ data }) =
   const mappedData = data.members.map(mapMembers).filter(p => !p.name.includes('[bot]'));
 
   return (
-    <div className="font-inter container mx-auto m-4 bg-gray-600" id={data.stat === "allTimes" ? "allTimes" : data.stat === "lastMonth" ? "lastMonth" : "lastWeek"}>
+    <div className="font-inter container mx-auto p-4 bg-gray-600" id={data.stat === "allTimes" ? "allTimes" : data.stat === "lastMonth" ? "lastMonth" : "lastWeek"}>
       <div className="flex flex-row justify-center">
-        <h4 className="text-gray-400 mb-4 text-center ml-10">{formatSinceAndUntil(data.since, data.until)}</h4>
         <h4 className='text-center'>{data.stat === "allTimes" ? "All Times" : data.stat === "lastMonth" ? "Last Month" : "Last Week"}</h4>
+        <h4 className="text-gray-400 mb-4 text-center ml-10">{formatSinceAndUntil(data.since, data.until)}</h4>
       </div>
       <ul className="grid grid-cols-3 gap-2">
         {mappedData.filter(p => p.score).map((data, ind) => (
@@ -73,7 +73,7 @@ const DisplayPerson2: React.FC<PersonPlace> = ({ data, place }) => {
     .map(([key]) => key.replace(/([A-Z])/g, ' $1').replace(/^./, str => str.toUpperCase()));
 
   return (
-    <div className="max-w-sm mx-auto bg-white shadow-lg rounded-lg overflow-hidden h-full">
+    <div className="max-w-sm mx-auto bg-white shadow-lg rounded-lg overflow-hidden h-full ">
       <div className="flex items-center px-6 py-4 bg-gray-900">
         <img className="h-12 w-12 rounded-full object-cover" src={data.avatar_url} alt="avatar" width={48} height={48} />
         <div className="ml-4">
